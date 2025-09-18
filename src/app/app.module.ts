cfa7362151210c36b8254f645d4cbf849a05e3b4
Thibaut LEFRANCOIS <thibaut.lfr@gmail.com>
2025-09-18 19:13:48 +0200

    feat: add chart component with pie chart implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BaseChartDirective, provideCharts, withDefaultRegisterables } from 'ng2-charts';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChartComponent } from './components/chart/chart.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, NotFoundComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
-  providers: [],
+  declarations: [AppComponent, HomeComponent, NotFoundComponent, ChartComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, CommonModule, BaseChartDirective],
+  providers: [provideCharts(withDefaultRegisterables())],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
